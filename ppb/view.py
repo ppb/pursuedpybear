c16633e01781e582034c5ffc16603f2ddab87b4a
Piper Thunstrom <pathunstrom@gmail.com>
2016-04-03 19:52:31 -0400

    Started physics module, added FPS calculator to view
    Added circle collision and an abstract collision class.
    
    Added a calculator for getting the average FPS over a single run time.

--- ppb/view.py
+++ ppb/view.py
@@ -1,4 +1,4 @@
-from ppb.event import Tick
+from ppb.event import Tick, Quit
 from ppb.vmath import Vector2 as Vector
 
 
@@ -8,9 +8,11 @@ class View(object):
         self.render_wait = 1 / fps
         self.countdown = self.render_wait
         scene.subscribe(Tick, self.tick)
+        scene.subscribe(Quit, self.quit)
         self.display = display
         self.layers = [set()]
         self.hw = hardware
+        self.fps = [[]]
 
     def render(self):
         for layer in self.layers:
@@ -20,10 +22,11 @@ class View(object):
         self.hw.draw_screen()
 
     def tick(self, event):
+        self.calculate_fps(event.sec)
         self.countdown += -1 * event.sec
         if self.countdown <= 0:
             self.render()
-            self.countdown = self.render_wait
+            self.countdown += self.render_wait
 
     def add(self, sprite, layer=0):
         """
@@ -54,6 +57,19 @@ class View(object):
     def change_layer(self, sprite, layer):
         self.remove(sprite)
         self.add(sprite, layer)
+
+    def calculate_fps(self, time):
+        self.fps[0].append(time)
+        for index, rates in enumerate(self.fps):
+            if len(rates) == 10:
+                average = sum(rates) / 10.0
+                try:
+                    self.fps[index + 1].append(average)
+                except IndexError:
+                    self.fps.append([average])
+
+    def quit(self, _):
+        print("FPS: {}".format(1 / (sum(self.fps[-1]) / len(self.fps[-1]))))
 
 
 class Sprite(object):
