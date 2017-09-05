99c9fcae26fe31698a64109b672d7008c4048f67
Piper Thunstrom <pathunstrom@gmail.com>
2017-09-04 21:59:08 -0400

    Fixes bug that requires always updating sprite.dirty to true.

--- ppb/scenes.py
+++ ppb/scenes.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from itertools import chain
+
 from ppb.abc import Scene
 from pygame import MOUSEBUTTONUP, QUIT
 from pygame.sprite import LayeredDirty
@@ -9,6 +11,8 @@ class BaseScene(Scene):
     def __init__(self, engine, **kwargs):
         super().__init__(engine)
         self.background_color = kwargs.get('background_color', (0, 0, 55))
+        engine.display.fill(self.background_color)
+        self.background = engine.display.copy()
         self.groups = defaultdict(LayeredDirty)
         self.callback_map = {
             QUIT: self.__quit__,
@@ -17,9 +21,8 @@ class BaseScene(Scene):
 
     def render(self):
         window = self.engine.display
-        window.fill(self.background_color)
-        for group in self.groups.values():
-            group.draw(window)
+        rects = [g.draw(window, self.background) for g in self.groups.values()]
+        return chain(*rects)
 
     def handle_event(self, event):
         self.callback_map.get(event.type, self.__null__)(event)
