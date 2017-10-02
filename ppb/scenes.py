24424d4ea6720adf17073b2dc93e55b07689e698
Cnidarias <pythonmolurus95@gmail.com>
2017-10-02 13:08:13 +0200

    Added pydoc to ppb/scenes.py

--- ppb/scenes.py
+++ ppb/scenes.py
@@ -9,6 +9,13 @@ from pygame.sprite import LayeredDirty
 class BaseScene(Scene):
 
     def __init__(self, engine, **kwargs):
+        r"""
+        Initializes the BaseScene
+
+        :param engine: The base engine which is to be used to render this scene
+        :keyword kwargs:
+            * *background_color* -- RGB Tuple specifying the background color for the scene
+        """
         super().__init__(engine)
         self.background_color = kwargs.get('background_color', (0, 0, 55))
         engine.display.fill(self.background_color)
@@ -20,33 +27,65 @@ class BaseScene(Scene):
         }
 
     def render(self):
+        """
+        Renders all the objects in the groups
+
+        :return: Iterable representing all the drawn rectangles
+        """
         window = self.engine.display
         rects = [g.draw(window, self.background) for g in self.groups.values()]
         return chain(*rects)
 
     def handle_event(self, event):
+        """
+        Calls the specified callback function from the callback map
+
+        :param event: The event that is supposed to be handled
+        """
         self.callback_map.get(event.type, self.__null__)(event)
 
     def publish_event(self, event):
         pass
 
     def simulate(self, time_delta: float):
+        """
+        Update all objects in the group calling their update function
+
+        :param time_delta: Time that has passed since the last update
+        """
         for group in self.groups.values():
             group.update(time_delta)
 
     def change(self):
         """
         Default case, override in subclass as necessary.
+
+        :return: Tuple specifying if the scene is running and what the scene class is
         """
         return self.running, {"scene_class": self.next}
 
     def __null__(self, event):
+        """
+        Does nothing
+
+        :param event: unused
+        """
         pass
 
     def __quit__(self, event):
+        """
+        Quits the current scene
+
+        :param event: unused
+        """
         _ = event
         self.running = False
         self.quit = True
 
     def __mouse_up__(self, event):
+        """
+        Does nothing
+
+        :param event: unused
+        """
         pass
