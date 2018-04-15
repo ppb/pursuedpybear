8112454f662fecd867d8781ab37a6c2d97eed264
Piper Thunstrom <pathunstrom@gmail.com>
2018-04-15 12:35:40 -0400

    Add docstrings

--- ppb/scenes.py
+++ ppb/scenes.py
@@ -39,16 +39,55 @@ class BaseScene(Scene):
         return self.running, {"scene_class": self.next}
 
     def add(self, game_object: Hashable, tags: Iterable=())-> None:
+        """
+        Add a game_object to the scene.
+
+        game_object: Any GameObject object. The item to be added.
+        tags: An iterable of Hashable objects. Values that can be used to
+              retrieve a group containing the game_object.
+
+        Examples:
+            scene.add(MyGameObject())
+
+            scene.add(MyGameObject(), tags=("red", "blue")
+        """
         self.game_objects.add(game_object, tags)
 
     def get(self, *, kind: Type=None, tag: Hashable=None, **kwargs) -> Iterator:
+        """
+        Get an iterator of GameObjects by kind or tag.
+
+        kind: Any type. Pass to get a subset of contained GameObjects with the
+              given type.
+        tag: Any Hashable object. Pass to get a subset of contained GameObjects
+             with the given tag.
+
+        Pass both kind and tag to get objects that are both that type and that
+        tag.
+
+        Examples:
+            scene.get(type=MyGameObject)
+
+            scene.get(tag="red")
+
+            scene.get(type=MyGameObject, tag="red")
+        """
         return self.game_objects.get(kind=kind, tag=tag, **kwargs)
 
     def remove(self, game_object: Hashable) -> None:
+        """
+        Remove the given object from the scene.
+
+        game_object: A game object.
+
+        Example:
+            scene.remove(my_game_object)
+        """
         self.game_objects.remove(game_object)
 
 
 class GameObjectContainer:
+    """A container for game objects."""
 
     def __init__(self):
         self.all = set()
@@ -59,12 +98,42 @@ class GameObjectContainer:
         return item in self.all
 
     def add(self, game_object: Hashable, tags: Iterable[Hashable]=()) -> None:
+        """
+        Add a game_object to the container.
+
+        game_object: Any Hashable object. The item to be added.
+        tags: An iterable of Hashable objects. Values that can be used to
+              retrieve a group containing the game_object.
+
+        Examples:
+            container.add(MyObject())
+
+            container.add(MyObject(), tags=("red", "blue")
+        """
         self.all.add(game_object)
         self.kinds[type(game_object)].add(game_object)
         for tag in tags:
             self.tags[tag].add(game_object)
 
     def get(self, *, kind: Type=None, tag: Hashable=None, **_) -> Iterator:
+        """
+        Get an iterator of objects by kind or tag.
+
+        kind: Any type. Pass to get a subset of contained items with the given
+              type.
+        tag: Any Hashable object. Pass to get a subset of contained items with
+             the given tag.
+
+        Pass both kind and tag to get objects that are both that type and that
+        tag.
+
+        Examples:
+            container.get(type=MyObject)
+
+            container.get(tag="red")
+
+            container.get(type=MyObject, tag="red")
+        """
         if kind is None and tag is None:
             raise TypeError("get() takes at least one keyword-only argument. 'kind' or 'tag'.")
         kinds = self.kinds[kind] or self.all
@@ -72,6 +141,14 @@ class GameObjectContainer:
         return (x for x in kinds.intersection(tags))
 
     def remove(self, game_object: Hashable) -> None:
+        """
+        Remove the given object from the container.
+
+        game_object: A hashable contained by container.
+
+        Example:
+            container.remove(myObject)
+        """
         self.all.remove(game_object)
         self.kinds[type(game_object)].remove(game_object)
         for s in self.tags.values():
