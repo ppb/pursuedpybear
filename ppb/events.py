d0689794143f735844178a5563d88f0d30c236b5
Piper Thunstrom <pathunstrom@gmail.com>
2018-08-22 11:53:29 -0700

    Precompile regex lookups.

--- ppb/events.py
+++ ppb/events.py
@@ -11,10 +11,12 @@ __all__ = (
     'Update',
 )
 
+boundaries_finder = re.compile('(.)([A-Z][a-z]+)')
+boundaries_finder_2 = re.compile('([a-z0-9])([A-Z])')
 
 def camel_to_snake(txt):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', txt)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+    s1 = boundaries_finder.sub(r'\1_\2', txt)
+    return boundaries_finder_2.sub(r'\1_\2', s1).lower()
 
 
 class EventMixin:
