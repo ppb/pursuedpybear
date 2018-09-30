fe9a22ac056571305a6b865595973295011ee2b6
Jamie Bliss <astronouth7303@gmail.com>
2018-09-29 21:10:31 -0400

    Add support for setting the window title

--- ppb/systems.py
+++ ppb/systems.py
@@ -125,14 +125,17 @@ class Quitter(System):
 
 class Renderer(System):
 
-    def __init__(self, resolution=default_resolution, **kwargs):
+    def __init__(self, resolution=default_resolution, window_title=None, **kwargs):
         self.resolution = resolution
         self.resources = {}
         self.window = None
+        self.window_title = window_title
 
     def __enter__(self):
         pygame.init()
         self.window = pygame.display.set_mode(self.resolution)
+        if self.window_title is not None:
+            pygame.display.set_caption(self.window_title)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         pygame.quit()
