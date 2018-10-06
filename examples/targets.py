65c41c79d63a116b3079b85687cb35d770a2ede8
Jamie Bliss <astronouth7303@gmail.com>
2018-10-05 20:22:54 -0400

    Basic layout

--- examples/targets.py
+++ examples/targets.py
@@ -1,107 +1,49 @@
 import logging
-
-from ppb import engine
-from ppb import hw as hardware
-from ppb.utilities import Publisher
-from ppb.event import (Quit,
-                       Tick,
-                       MouseButtonDown,
-                       ObjectCreated,
-                       ObjectDestroyed,
-                       Collision)
-from ppb.components import models
-from ppb.components.controls import control_move, emit_object
-from ppb.physics import Physics
-from ppb.vmath import Vector2 as Vector
+import ppb
+from ppb import Vector
 
 
-publisher = Publisher()
+class Player(ppb.BaseSprite):
+    def on_key_press(self, event, signal):
+        ... # Set movement, using WASD
+
+    def on_button_press(self, event, signal):
+        ... # Create bullet
+
+    def on_update(self, update, signal):
+        ... # Execute movement
 
 
-class Player(models.GameObject,
-             models.Mobile,
-             models.HardwarePrimitive,
-             models.Collider):
+class Bullet(ppb.BaseSprite):
+    def on_update(self, update, signal):
+        ... # Move, check for out-of-bounds
+
+
+class Target(ppb.BaseSprite):
     pass
 
 
-class Bullet(models.GameObject,
-             models.Mobile,
-             models.HardwarePrimitive,
-             models.Collider):
-    pass
+class GameScene(ppb.BaseScene):
+    def __init__(self, *p, **kw):
+        super().__init__(*p, **kw)
 
+        # Set up sprites
+        self.add(Player(pos=Vector(0, 0)), tags=['player'])
 
-class Target(models.GameObject, models.HardwarePrimitive, models.Collider):
-    pass
+        # 5 targets in x = -3.75 -> 3.75, give 1/5 margin between targets 
+        for x in (-2.4, -1.2, 0, 1.2, 2.4):
+            self.add(Target(pos=Vector(x, 1.875)), tags=['target'])
 
-
-def hit(self, collision):
-    is_hit = False
-    other = None
-    for item in collision.members:
-        if item == self:
-            is_hit = True
-        else:
-            other = item
-    if is_hit:
-        if isinstance(other, Bullet) or isinstance(other, Target):
-            self.kill()
-
-
-def subscribe(event):
-    for command in event.commands:
-        publisher.subscribe(*command)
-
-
-def unsubscribe(event):
-    for command in event.commands:
-        publisher.unsubscribe(*command)
+    def on_update(self, update, signal):
+        ... # Check for collisions between bullets and targets
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
-    hardware.choose("pygame")
-    hardware.init((600, 400), "Targets")
-    publisher.subscribe(Quit, hardware.quit)
-    publisher.subscribe(ObjectCreated, subscribe)
-    publisher.subscribe(ObjectDestroyed, unsubscribe)
-    publisher.subscribe(Tick, hardware.update_input)
-    hardware.View(publisher,
-                  hardware.display(),
-                  30,
-                  hardware,
-                  hardware.image_primitive((0, 0, 0), (600, 400)))
-    Physics()
-
-    player_color = (128, 65, 40)
-    key_bindings = {"up": ord("w"),
-                    "down": ord("s"),
-                    "left": ord("a"),
-                    "right": ord("d")}
-    player_speed = 60
-    bullet_params = {"color": (255, 255, 255),
-                     "image_size": 4,
-                     "hardware": hardware,
-                     "behaviors": [(Collision, hit)]}
-    controls = [(Tick, control_move(speed=player_speed,
-                                    **key_bindings)),
-                (MouseButtonDown, emit_object(Bullet, bullet_params, 1, 120))]
-    Player(color=player_color,
-           hardware=hardware,
-           pos=Vector(300, 200),
-           behaviors=controls,
-           radius=10)
-    target_color = (0, 123, 0)
-    step = 600 / 6
-    for x in range(5):
-        Target(color=target_color,
-               image_size=20,
-               hardware=hardware,
-               pos=Vector(step * (x + 1), 50),
-               radius=10,
-               behaviors=[(Collision, hit)])
-    engine.run(publisher)
+    ppb.run(GameScene,
+        logging_level=logging.DEBUG,
+        window_title="Targets",
+        resolution=(600, 400),
+    )
 
 if __name__ == "__main__":
     main()
