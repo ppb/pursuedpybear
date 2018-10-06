8c900ea5f82a3eec0aa798960b4da63b1bc5fb09
Jamie Bliss <astronouth7303@gmail.com>
2018-10-06 16:56:49 -0400

    Disallow abstract instantiation, fix regular instantiation

--- ppb/flags.py
+++ ppb/flags.py
@@ -10,28 +10,26 @@ __all__ = 'Flag', 'DoNotRender'
 
 
 class FlagMeta(type):
-    _instance = None
     def __new__(mcls, *p, abstract=False, **kw):
         cls = super().__new__(mcls, *p, **kw)
-        print("__new__", mcls, cls, p, kw)
         if abstract:
+            cls._instance = ...
             return cls
         else:
+            cls._instance = None
             return cls()
 
-    def __call__(cls, *p, **kw):
+    def __call__(cls):
         if cls._instance is None:
-            cls._instance = super().__call__(*p, **kw)
+            cls._instance = type.__call__(cls)
+        elif cls._instance is ...:
+            raise TypeError("Cannot instantiate abstract flags")
         return cls._instance
 
 
 class Flag(metaclass=FlagMeta, abstract=True):
     """Inherit from Flag to make a simple class flag."""
-    def __new__(cls, *args, **kwargs):
-        return cls
 
 
 class DoNotRender(Flag):
     """Inform the renderer to ignore this object."""
-
-
--- tests/test_flags.py
+++ tests/test_flags.py
@@ -1,4 +1,5 @@
 import inspect
+import pytest
 from ppb.flags import Flag
 
 
@@ -14,3 +15,31 @@ def test_subclass():
      class TestFlag(SpecialFlag): pass
 
      assert isinstance(TestFlag, SpecialFlag)
+
+
+def test_instantiate():
+    class TestFlag(Flag): pass
+
+    with pytest.raises(TypeError):
+        TestFlag()
+
+    dup = type(TestFlag)()
+    assert dup is TestFlag
+
+
+def test_abstract_instantiate():
+     class SpecialFlag(Flag, abstract=True): pass
+
+     with pytest.raises(TypeError):
+        SpecialFlag()
+
+
+def test_comparison():
+     class FlagA(Flag): pass
+     class FlagB(Flag): pass
+
+     assert FlagA is FlagA
+     assert FlagB is not FlagA
+     assert FlagA == FlagA
+     assert FlagB != FlagA
+
