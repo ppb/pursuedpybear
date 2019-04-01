6eb410b7d665c263e2f5b67dfbfd4b1d8dffed4e
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-04-01 23:31:09 +0200

    setup.py: Move all configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup
 
 
-def readme():
-    with open('README.md') as file:
-        return file.read()
-
 def requirements(section=None):
+    """Helper for loading dependencies from requirements files."""
     if section is None:
         filename = "requirements.txt"
     else:
@@ -14,29 +11,9 @@ def requirements(section=None):
     with open(filename) as file:
         return [line.strip() for line in file]
 
+
+# See setup.cfg for the actual configuration.
 setup(
-    name='ppb',
-    version='0.5.0',
-    packages=['ppb', 'ppb.systems'],
     install_requires=requirements(),
-    setup_requires=['pytest-runner'],
     tests_require=requirements('tests'),
-    python_requires=">=3.6",
-    url='https://github.com/ppb/pursuedpybear',
-    license='Artistic-2.0',
-    author='Piper Thunstrom',
-    author_email='pathunstrom@gmail.com',
-    description='An Event Driven Python Game Engine',
-    long_description=readme(),
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Education",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Artistic License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.7",
-        "Topic :: Games/Entertainment",
-        "Topic :: Software Development :: Libraries",
-    ]
 )
