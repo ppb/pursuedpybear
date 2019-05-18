354f1ce5ed03889fc17edaa71e92a67edcde9a0e
Jamie Bliss <jamie@ivyleav.es>
2019-05-17 22:01:05 -0400

    Add a feature for two-phase updates

---
+++ ppb/features/twophase.py
@@ -0,0 +1,50 @@
+"""
+A system for two phase updates: Update, and Commit.
+"""
+from dataclasses import dataclass
+from ppb.systems import System
+from ppb.events import EventMixin
+
+__all__ = 'Commit',
+
+
+@dataclass
+class Commit:
+    """
+    Fired after Update.
+    """
+
+
+class TwoPhaseSystem(System):
+    """
+    Produces the Commit event.
+    """
+
+    def on_update(self, event, signal):
+        signal(Commit())
+
+
+class TwoPhaseMixin(EventMixin):
+    """
+    Mixin to apply to objects to handle two phase updates.
+    """
+
+    __staged_changes = None
+
+    def stage_changes(self, **kwargs):
+        """
+        Stage changes for the next commit.
+
+        These are just properties on the current object to update.
+        """
+        if self.__staged_changes is None:
+            self.__staged_changes = {}
+        self.__staged_changes.update(kwargs)
+
+    def do_commit(self, event, signal):
+        """
+        Commit changes previously staged.
+        """
+        changes, self.__staged_changes = self.__staged_changes, {}
+        for name, value in changes.items():
+            setattr(self, name, value)
