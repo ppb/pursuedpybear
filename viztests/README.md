9d3d829c7b6e9f470abee236a02eb28075de22cd
Jamie Bliss <jamie@ivyleav.es>
2019-07-20 17:10:47 -0400

    Initial implementation.

---
+++ viztests/README.md
@@ -0,0 +1,6 @@
+Visual Tests
+============
+
+This is a test suite of manual tests, meant to help with the difficulty of
+thorough automated testing of graphical software.
+
---
+++ viztests/angles.py
@@ -0,0 +1,31 @@
+"""
+Tests rotation vs Vector angles
+
+The center sprite should always face the orbiting sprite
+"""
+import ppb
+
+ROTATION_RATE = 90
+
+
+class CenterSprite(ppb.BaseSprite):
+    image = ppb.Image('player.png')
+
+    def on_update(self, event, signal):
+        self.rotation += ROTATION_RATE * event.time_delta
+
+
+class OrbitSprite(ppb.BaseSprite):
+    position = ppb.Vector(0, -2)
+    image = ppb.Image('target.png')
+
+    def on_update(self, event, signal):
+        self.position = self.position.rotate(ROTATION_RATE * event.time_delta)
+
+
+def setup(scene):
+    scene.add(CenterSprite())
+    scene.add(OrbitSprite())
+
+
+ppb.run(setup)
