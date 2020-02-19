e2a1f5689dfbcd2b2fa06da6f4ecc38088a6fc9f
Jamie Bliss <jamie@ivyleav.es>
2020-02-19 16:31:28 -0500

    Cirrus: Use Python script to download & install wheels
    Co-authored-by: Nicolas Braud-Santoni <nicolas@braud-santoni.eu>

---
+++ .ci/install-wheels.py
@@ -0,0 +1,20 @@
+import glob
+import io
+import os
+import subprocess
+from urllib.request import urlopen
+import zipfile
+
+CIRRUS_BUILD_ID = os.environ['CIRRUS_BUILD_ID']
+ARTIFACTS_URL = f"https://api.cirrus-ci.com/v1/artifact/build/{CIRRUS_BUILD_ID}/build/dist.zip"
+
+with urlopen(ARTIFACTS_URL) as resp:
+    zipdata = resp.read()
+
+with zipfile.ZipFile(io.BytesIO(zipdata)) as zf:
+    zf.extractall()
+
+subprocess.run(
+    ['pip', 'install', *glob.glob('dist/*.whl')],
+    check=True
+)
