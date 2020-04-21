a67d5f7f1b01e78a0c79d56806fdf2e37d7bc6bc
Jamie Bliss <jamie@ivyleav.es>
2020-04-20 22:37:31 -0400

    _sdl_utils.py: Add ttf calling

--- ppb/systems/_sdl_utils.py
+++ ppb/systems/_sdl_utils.py
@@ -17,6 +17,12 @@ from sdl2.sdlimage import (
     IMG_GetError, IMG_SetError,  # https://www.libsdl.org/projects/SDL_image/docs/SDL_image_43.html#SEC43
 )
 
+from sdl2.sdlttf import (
+    # https://www.libsdl.org/projects/SDL_ttf/docs/SDL_ttf_6.html#SEC6
+    TTF_GetError, TTF_SetError,
+)
+
+
 from ppb.systemslib import System
 
 
@@ -113,3 +119,24 @@ def img_call(func, *pargs, _check_error=None, **kwargs):
         return rv
 
 
+class TtfError(SdlError):
+    pass
+
+
+def ttf_call(func, *pargs, _check_error=None, **kwargs):
+    """
+    Wrapper for calling SDL functions for handling errors.
+
+    If _check_error is given, called with the return value to check for errors.
+    If _check_error returns truthy, an error occurred.
+
+    If _check_error is not given, it is assumed that a non-empty error from
+    Mix_GetError indicates error.
+    """
+    TTF_SetError(b"")
+    rv = func(*pargs, **kwargs)
+    err = TTF_GetError()
+    if (_check_error(rv) if _check_error else err):
+        raise SdlError(f"Error calling {func.__name__}: {err.decode('utf-8')}")
+    else:
+        return rv
