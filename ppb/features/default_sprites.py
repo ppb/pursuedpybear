280e0f2a6d0cb2aa90a78a288b3e28489b3eb329
Serin Delaunay <serin.delaunay@gmail.com>
2020-05-07 00:43:37 +0100

    Avoid zero division in TargetSprite.

--- ppb/features/default_sprites.py
+++ ppb/features/default_sprites.py
@@ -7,5 +7,10 @@ class TargetSprite(ppb.BaseSprite):
     speed = 1.0
 
     def on_update(self, update_event, signal):
-        direction = (self.position - self.target).normalize()
-        self.position += direction * self.speed * update_event.time_delta
+        offset = self.position - self.target
+        distance_this_tick = self.speed * update_event.time_delta
+        if offset.length <= distance_this_tick:
+            self.position = self.target
+        else:
+            direction = offset.normalize()
+            self.position += direction * self.speed * update_event.time_delta
