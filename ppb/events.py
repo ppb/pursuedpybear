cf88a9a32ce6fd82dd3a0ce6afa51dae4913a4e9
Piper Thunstrom <pathunstrom@gmail.com>
2020-05-07 22:20:43 -0400

    Updates events docstrings.

--- ppb/events.py
+++ ppb/events.py
@@ -1,5 +1,25 @@
 """
-A big pile of the events defined in PPB.
+Events are the primary way objects in ppb communicate. This module contains the
+events defined by various systems in ppb.
+
+To respond to an event your event handler should be snake_cased
+``on_event_name`` and accept an event instance and a signal function as
+parameters. Example: ::
+
+   class MySprite(Sprite):
+       def on_update(self, event: Update, signal):
+           . . .
+
+The :meth:`~ppb.GameEngine.signal` function accepts one parameter: an
+instance of an event class. You are not limited to predefined event types, but
+can provide arbitrary instances.
+
+Events as defined here are :func:`dataclasses.dataclass` but :mod:`ppb` does
+not expect dataclasses, they are just a simple way to quickly define new events.
+The name of the handler will always be based on the name of the class, with the
+TitleCase name of the class converted to on_event_name function. The instance
+passed to :meth:`~ppb.GameEngine.signal` will be passed to all the event
+handlers as the event parameter.
 """
 from dataclasses import dataclass
 from typing import Any
@@ -37,29 +57,37 @@ import ppb
 @dataclass
 class ButtonPressed:
     """
-    Fired when a button is pressed
+    A mouse button was pressed.
+
+    The button is a :class:`~ppb.buttons.MouseButton` instance.
+
+    This represents the button in the active state. For acting when a button
+    is released see :class:`ButtonReleased`.
     """
     button: MouseButton
     position: Vector  # Scene position
-    # TODO: Add frame position
     scene: BaseScene = None
 
 
 @dataclass
 class ButtonReleased:
     """
-    Fired when a button is released
+    A mouse button was released.
+
+    The button is a :class:`~ppb.buttons.MouseButton` instance.
+
+    This represents the button in the inactive state. For acting when a button
+    is clicked see :class:`ButtonPressed`.
     """
-    button: MouseButton
-    position: Vector  # Scene position
-    # TODO: Add frame position
-    scene: BaseScene = None
+    button: MouseButton  #: A mouse button: Primary, Secondary, or Tertiary
+    position: Vector  #: The game-world position of the event.
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class StartScene:
     """
-    Fired to start a new scene.
+    An object requested a new scene to be started.
 
     new_scene can be an instance or a class. If a class, must include kwargs.
     If new_scene is an instance kwargs should be empty or None.
@@ -73,65 +101,109 @@ class StartScene:
     Examples:
         * `signal(new_scene=StartScene(MyScene(player=player))`
         * `signal(new_scene=StartScene, kwargs={"player": player}`
+
+    .. warning::
+       In general, you should not respond to StartScene, if you want to respond to
+       a new scene, see :class:`SceneStarted`.
     """
-    new_scene: Union[BaseScene, Type[BaseScene]]
-    kwargs: Dict[str, Any] = None
-    scene: BaseScene = None
+    new_scene: Union[BaseScene, Type[BaseScene]]  #: A :class:`~ppb.scenes.BaseScene` class or instance
+    kwargs: Dict[str, Any] = None  #: Keyword arguments to be passed to a scene type.
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class KeyPressed:
-    key: KeyCode
-    mods: Set[KeyCode]
-    scene: BaseScene = None
+    """
+    A keyboard key was pressed.
+
+    The buttons are defined in :mod:`ppb.keycodes`.
+
+    This represents the key entering an active state, to respond to when a key
+    is released see :class:`KeyReleased`.
+    """
+    key: KeyCode  #: A :class:`~ppb.keycodes.KeyCode` flag.
+    mods: Set[KeyCode]  #: A set of :class:`KeyCodes <ppb.keycodes.KeyCode>`
+    scene: BaseScene = None  #: The currently running scene
 
 
 @dataclass
 class KeyReleased:
-    key: KeyCode
-    mods: Set[KeyCode]
-    scene: BaseScene = None
+    """
+    A keyboard key was released.
+
+    The buttons are defined in :mod:`ppb.keycodes`.
+
+    This represents the key entering an inactive state, to respond to when a key
+    is pressed see :class:`KeyPressed`.
+    """
+    key: KeyCode  #: A :class:`~ppb.keycodes.KeyCode` flag.
+    mods: Set[KeyCode]  #: A set of :class:`KeyCodes <ppb.keycodes.KeyCode>`
+    scene: BaseScene = None  #: The currently running scene
 
 
 @dataclass
 class MouseMotion:
-    """An event to represent mouse motion."""
-    position: Vector
-    screen_position: Vector
-    delta: Vector
-    buttons: Collection[MouseButton]
-    scene: BaseScene = None
+    """
+    The mouse moved.
+
+    If something should be tracking the mouse, this is the event to listen to.
+    """
+    position: Vector  #: The game-world location of the mouse cursor.
+    screen_position: Vector  #: The screen space location of the mouse cursor.
+    delta: Vector  #: The change in position since the last MouseMotion event.
+    buttons: Collection[MouseButton]  #: The state of the mouse buttons.
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class PreRender:
     """
-    Fired before rendering.
+    The :class:`~ppb.Renderer` is preparing to render.
+
+    :class:`PreRender` is called before every frame is rendered. Things that are
+    strictly for display purposes (like the text of a score board or the
+    position of the camera) should happen ``on_pre_render``.
     """
-    scene: BaseScene = None
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class Quit:
     """
-    Fired on an OS Quit event.
+    A request to quit the program.
 
-    You may also fire this event to stop the engine.
+    Fired in response to a close request from the OS, but can be signaled from
+    inside one of your handlers to end the program.
+
+    For example: ::
+
+       def on_update(self, event, signal):
+           signal(Quit())
+
+    This will stop the engine.
+
+    Respond with ``on_quit`` to perform any shut down tasks (like saving data.)
     """
-    scene: BaseScene = None
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class Render:
     """
-    Fired at render.
+    The :class:`~ppb.systems.renderer.Renderer` is rendering.
+
+    .. warning::
+       In general, responses to Render will not be reflected until the next
+       render pass. If you want changes to effect this frame, see
+       :class:`PreRender`
     """
-    scene: BaseScene = None
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class ReplaceScene:
     """
+    An object requested a
     Fired to replace the current scene with a new one.
 
     new_scene can be an instance or a class. If a class, must include kwargs.
@@ -146,108 +218,129 @@ class ReplaceScene:
     Examples:
         * `signal(new_scene=ReplaceScene(MyScene(player=player))`
         * `signal(new_scene=ReplaceScene, kwargs={"player": player}`
+
+    .. warning::
+       In general, you should not respond to ReplaceScene, if you want to
+       respond to a new scene, see :class:`SceneStarted`.
     """
-    new_scene: Union[BaseScene, Type[BaseScene]]
-    kwargs: Dict[str, Any] = None
-    scene: BaseScene = None
+    new_scene: Union[BaseScene, Type[BaseScene]]  #: A :class:`~ppb.scenes.BaseScene` class or instance
+    kwargs: Dict[str, Any] = None  #: Keyword arguments to be passed to a scene type.
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class SceneContinued:
     """
-    Fired when a paused scene continues.
+    A scene that had been paused has continued.
 
     This is delivered to a scene as it resumes operation after being paused via
     a ScenePaused event.
 
-    From the middle of the event lifetime that begins with SceneStarted.
+    From the middle of the event lifetime that begins with
+    :class:`SceneStarted`.
     """
-    scene: BaseScene = None
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class SceneStarted:
     """
-    Fired when a scene starts.
+    A new scene has started running.
 
     This is delivered to a Scene shortly after it starts. The beginning of the
-    scene lifetime, ended with SceneStopped, paused with ScenePaused, and
-    resumed from a pause with SceneContinued.
+    scene lifetime, ended with :class:`SceneStopped`, paused with
+    :class:`ScenePaused`, and resumed from a pause with :class:`SceneContinued`.
     """
-    scene: BaseScene = None
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class SceneStopped:
     """
-    Fired when a scene stops.
+    A scene is being stopped and will no longer be available.
 
     This is delivered to a scene and it's objects when a StopScene or
     ReplaceScene event is sent to the engine.
 
-    The end of the scene lifetime, started with SceneStarted.
+    The end of the scene lifetime, started with :class:`SceneStarted`.
     """
-    scene: BaseScene = None
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class ScenePaused:
     """
-    Fired when a scene pauses.
+    A scene that is running is being paused to allow a new scene to run.
 
-    This is delivered to a scene about to be paused when a StartScene event is
-    sent to the engine. When this scene resumes it will receive a
-    SceneContinued event.
+    This is delivered to a scene about to be paused when a :class:`StartScene`
+    event is sent to the engine. When this scene resumes it will receive a
+    :class:`SceneContinued` event.
 
-    A middle event in the scene lifetime, started with SceneStarted.
+    A middle event in the scene lifetime, started with :class:`SceneStarted`.
     """
-    scene: BaseScene = None
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class StopScene:
     """
-    Fired to stop a scene.
+    An object has requested the current scene be stopped.
 
-    Before the scene stops, a SceneStopped event will be fired. Any events
-    signaled in response will be delivered to the previous scene if it exists.
+    Before the scene stops, a :class:`SceneStopped` event will be fired. Any
+    events signaled in response will be delivered to the previous scene if it
+    exists.
 
-    If there is a paused scene on the stack, a SceneContinued event will be
-    fired after the responses to the SceneStopped event.
+    If there is a paused scene on the stack, a :class:`SceneContinued` event
+    will be fired after the responses to the :class:`SceneStopped` event.
+
+    .. warning::
+       In general, you should not respond to StopScene, if you want to respond
+       to a scene ending, see :class:`SceneStopped`.
     """
-    scene: BaseScene = None
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class Idle:
     """
-    An engine plumbing event to pump timing information to subsystems.
+    A complete loop of the :class:`~ppb.engine.GameEngine` main loop has
+    finished.
+
+    This is an engine plumbing event to pump timing information to subsystems.
     """
-    time_delta: float
-    scene: BaseScene = None
+    time_delta: float  #: Seconds since last Idle.
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class Update:
     """
-    Fired on game tick
+    A simulation tick.
+
+    Respond via on_update to advance the simulation of your game objects.
     """
-    time_delta: float
-    scene: BaseScene = None
+    time_delta: float  #: Seconds since last Update
+    scene: BaseScene = None  #: The currently running scene.
 
 
 @dataclass
 class PlaySound:
     """
-    Fire to start a sound playing.
+    An object requested a sound be played.
+
+    Signal in an event handler to have a sound played.
+
+    Example: ::
+       signal(PlaySound(my_sound))
     """
-    sound: 'ppb.assets.Asset'
+    sound: 'ppb.assets.Asset'  #: A :class:`~ppb.systems.sound.Sound` asset.
+
 
 @dataclass
 class AssetLoaded:
     """
-    Fired whenever an asset finished loading.
+    An asset has finished loading.
     """
-    asset: 'ppb.assets.Asset'
-    total_loaded: int
-    total_queued: int
+    asset: 'ppb.assets.Asset'  #: A :class:`~ppb.assetlib.Asset`
+    total_loaded: int  #: The total count of loaded assets.
+    total_queued: int  # The total number of requested assets.
