7252fe441925d562508f22f6cfbad1a741dc4c18
Piper Thunstrom <pathunstrom@gmail.com>
2020-05-09 14:00:22 -0400

    Adjust names of ordinals.

--- ppb/directions.py
+++ ppb/directions.py
@@ -7,11 +7,11 @@ Best used for the positions or facings of :class:`Sprites <ppb.Sprite>`.
 """
 from ppb_vector import Vector
 
-UP = Vector(0, 1).normalize()  #: Unit vector to the top of the screen from center.
-DOWN = Vector(0, -1).normalize()  #: Unit vector to the bottom of the screen from center.
-LEFT = Vector(-1, 0).normalize()  #: Unit vector to the left of the screen from center.
-RIGHT = Vector(1, 0).normalize()  #: Unit vector to the right of the screen from center.
-UP_AND_LEFT = (UP + LEFT).normalize()  #: Unit vector diagonally up and to the left of the screen from center.
-UP_AND_RIGHT = (UP + RIGHT).normalize()  #: Unit vector diagonally up and to the right of the screen from center.
-DOWN_AND_LEFT = (DOWN + LEFT).normalize()  #: Unit vector diagonally down and to the left of the screen from center.
-DOWN_AND_RIGHT = (DOWN + RIGHT).normalize()  #: Unit vector diagonally down and to the right of the screen from center.
+Up = Vector(0, 1).normalize()  #: Unit vector to the top of the screen from center.
+Down = Vector(0, -1).normalize()  #: Unit vector to the bottom of the screen from center.
+Left = Vector(-1, 0).normalize()  #: Unit vector to the left of the screen from center.
+Right = Vector(1, 0).normalize()  #: Unit vector to the right of the screen from center.
+UpAndLeft = (Up + Left).normalize()  #: Unit vector diagonally up and to the left of the screen from center.
+UpAndRight = (Up + Right).normalize()  #: Unit vector diagonally up and to the right of the screen from center.
+DownAndLeft = (Down + Left).normalize()  #: Unit vector diagonally down and to the left of the screen from center.
+DownAndRight = (Down + Right).normalize()  #: Unit vector diagonally down and to the right of the screen from center.
