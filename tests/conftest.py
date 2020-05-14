460ade983ce9b82ceda462cf23b844d745f4f1c0
Jamie Bliss <jamie@ivyleav.es>
2020-05-14 15:20:44 -0400

    conftest.py: Use relative sizing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
 from hypothesis import settings
-settings.register_profile("x-large", max_examples=10000)
-settings.register_profile("large", max_examples=1000)
-settings.register_profile("jumbo", max_examples=500)
+settings.register_profile("x-large", max_examples=100*settings.default.max_examples)
+settings.register_profile("large", max_examples=10*settings.default.max_examples)
+settings.register_profile("jumbo", max_examples=5*settings.default.max_examples)
