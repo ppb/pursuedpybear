99929d7e149a7ecaa8e3c2aa77924cad25c3d25a
Gideon Grinberg <49839886+Gideon357@users.noreply.github.com>
2020-10-18 10:56:38 -0400

    Remove theme

--- docs/conf.py
+++ docs/conf.py
@@ -39,8 +39,6 @@ html_theme_options = {
 # If your documentation needs a minimal Sphinx version, state it here.
 #
 # needs_sphinx = '1.0'
-import alabaster
-
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -87,7 +85,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -99,7 +97,7 @@ html_theme = "alabaster"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_css_files = ["center.css"]
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
